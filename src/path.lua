4b46010420fe935b2ab01252e3a1cc82b3f6c708
Daogang Tang <daogangtang@gmail.com>
2012-11-24 23:47:46 +0800

    add a pattern to normalizPath.
    Signed-off-by: Daogang Tang <daogangtang@gmail.com>

--- src/path.lua
+++ src/path.lua
@@ -2,7 +2,7 @@ module(..., package.seeall)
 
 function normalize(path)
 	local path = path:gsub('//+', '/')
-	if path:sub(-1) ~= '/' then
+	if path:sub(-1) ~= '/' and not path:find('.', 1, true) then
 		path = ('%s/'):format(path)
 	end
 	
